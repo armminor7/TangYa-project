5834da85cee37e93a897b97404fb7b1a18c1747f
Osca@r1010 <wasan.sr@ku.th>
2020-04-15 14:47:10 +0700

    add expand menu animation

--- FinTrans.js
+++ FinTrans.js
@@ -1,12 +1,21 @@
 const finTransMenu = document.querySelector('#fin-trans-menu');
 const finTransDiv = document.querySelector('.fin-trans');
 
+
 finTransMenu.addEventListener('click', ()=>{
 
     if (finTransDiv.style.display === 'none') {
         finTransDiv.style.display = 'flex';
+        let op = 0;
+
+        let fadeIn = setInterval(()=>{
+            finTransDiv.style.opacity = op;
+            op += 0.05;
+            if (op >= 1) {clearInterval(fadeIn)};
+        },15);
     } else {
         finTransDiv.style.display = 'none';
+        finTransDiv.style.opacity = '0';
     }
 
 });
